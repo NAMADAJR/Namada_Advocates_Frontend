d7a4fe05430272a57f4488d7f28e1e8a53a9fd38
Nam <namada871@gmail.com>
2025-03-31 22:22:19 +0300

    Initializes react app with vite and tailwind

---
+++ README.md
@@ -0,0 +1,9 @@
+# Namada & Co Advocates
+
+## Initilization
+- React with vite (Javascript)
+- Tailwind V3
+- NPM version 11.2.0
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-black h-screen flex justify-center items-center border-2 border-blue-500">
+      <h1 className="text-4xl text-blue-800">Namada & Co Advocates</h1>
+    </div>
+  )
+}
+
+export default App
+
