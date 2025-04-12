253d6b347f3b904fe28e9f40a857badd7e45979d
+ <+>
2025-04-12 21:42:54 +0300

    overall changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TeamsPage from './Components/Team'
 
 const App = () => {
   return (
-    <div className="bg-black h-screen flex justify-center items-center border-2 border-blue-500">
-      <h1 className="text-4xl text-blue-800">Namada & Co Advocates</h1>
+    <div>
+      <TeamsPage />
     </div>
   )
 }
